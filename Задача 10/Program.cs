f0f045bdfea24c51192f375b0cf892c82bdf3d55
Alexandra <Kaverzina0331@mail.ru>
2022-09-24 09:06:51 +0300

    Задача 10

---
+++ Задача 10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write ("Введите 3-х значное число: ");
+// 10. Напишите программу, которая принимает 
+//на вход трёхзначное число и на выходе 
+//показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+int Number = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(Number);
+Console.WriteLine("вторая цифра числа -> "+stringNumber[1]);
